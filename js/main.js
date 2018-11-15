bc2ce59db21142f5ee91044fc0c1400dadf721ab
iquach92 <iquach@icloud.com>
2018-11-15 18:38:09 +0200

    version 1

--- js/main.js
+++ js/main.js
@@ -1,20 +1,39 @@
-;(function() {
+;
+(function () {
     "use strict";
+    var startBtn = document.querySelector('.ba-lets-start');
+    startBtn.addEventListener('click', showForm);
+    var audio = document.getElementById('music');
     
-    var login = prompt('Кто пришел??(Login)', '???');
-    var password;
-    var showBlack = document.querySelector('.ba-show');
-    if(login === 'Админ'){
-        password = prompt('Пароль??', '');
-        if(password === 'Черный Властелин') {
-            alert('Добро пожаловать МОЙ ГОСПОДИН!');
-            showBlack.style.display = 'block';
+    function showForm() {
+        var login = prompt('Кто пришел??(Login)', '???');
+        var password;
+        var showBlack = document.querySelector('.ba-show');
+
+        if (login === 'Админ') {
+            password = prompt('Пароль??', '');
+
+            while (password !== 'Черный Властелин') {
+                if(password !== null){
+                    password = prompt('Пароль не верный!Пробуй еще...', '');
+                }
+            }
+            if (password === 'Черный Властелин') {
+                alert('Добро пожаловать МОЙ ГОСПОДИН!');
+                showBlack.style.display = 'block';
+                startBtn.style.display = 'none';
+                audio.play();
+
+            } else if(password === null) {
+                alert('Вход Отменен');
+            }
+        } else if (login === null) {
+            alert('Вход Отменен');
+
         } else {
-            alert('Пароль не верный');
+            alert('Я Вас не знаю!');
         }
 
-    } else {
-        alert('Я Вас не знаю!');
+    }
 
-    } 
 })();
